33cc795f8d262f4dbc53a8bc652cd9edc2b02abf
Görkem Narinoğlu <c1411041@student.cankaya.edu.tr>
2018-03-22 11:11:15 +0300

    s

---
+++ Source/VRELAX_IoR.Target.cs
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class VRELAX_IoRTarget : TargetRules
+{
+	public VRELAX_IoRTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+
+		ExtraModuleNames.AddRange( new string[] { "VRELAX_IoR" } );
+	}
+}
---
+++ Source/VRELAX_IoR/VRELAX_IoR.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VRELAX_IoR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRELAX_IoR, "VRELAX_IoR" );
